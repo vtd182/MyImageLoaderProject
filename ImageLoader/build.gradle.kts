1268eaeb7433437b824c6aa9fadcda71918e6da4
vtd182 <21120432@student.hcmus.edu.vn>
2025-10-21 11:21:33 +0700

    Revert "jacoco config"
    This reverts commit d80dd94b5b3d0a8929101df57cb7135db25dad21.

--- ImageLoader/build.gradle.kts
+++ ImageLoader/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
-    id("jacoco")
 }
 
 android {
@@ -10,6 +9,7 @@ android {
 
     defaultConfig {
         minSdk = 24
+
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
@@ -23,20 +23,12 @@ android {
             )
         }
     }
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-
     kotlinOptions {
         jvmTarget = "11"
-    }
-
-    testOptions {
-    unitTests.all {
-    it.useJUnitPlatform() // nếu bạn dùng JUnit5
-    }
     }
 }
 
@@ -56,56 +48,4 @@ dependencies {
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.1")
     testImplementation("org.robolectric:robolectric:4.12.2")
     testImplementation(kotlin("test"))
-}
-
-jacoco {
-    toolVersion = "0.8.11"
-}
-
-afterEvaluate {
-    tasks.named("testDebugUnitTest") {
-        finalizedBy("jacocoTestReport")
-    }
-}
-
-tasks.register<JacocoReport>("jacocoTestReport") {
-group = "verification"
-description = "Generate Jacoco coverage reports"
-
-reports {
-        xml.required.set(true)
-    html.required.set(true)
-csv.required.set(false)
-html.outputLocation.set(layout.buildDirectory.dir("reports/jacoco"))
-}
-
-    val debugTree = fileTree("$buildDir/tmp/kotlin-classes/debug") {
-        exclude(
-            "**/R.class",
-            "**/R$*.class",
-            "**/BuildConfig.*",
-            "**/Manifest*.*",
-            "**/*Test*.*"
-        )
-    }
-
-    val javaDebugTree = fileTree("$buildDir/intermediates/javac/debug/classes") {
-        exclude(
-            "**/R.class",
-            "**/R$*.class",
-            "**/BuildConfig.*",
-            "**/Manifest*.*",
-            "**/*Test*.*"
-        )
-    }
-
-    classDirectories.setFrom(files(debugTree, javaDebugTree))
-    sourceDirectories.setFrom(files("$projectDir/src/main/java", "$projectDir/src/main/kotlin"))
-    executionData.setFrom(fileTree(buildDir) {
-        include("jacoco/testDebugUnitTest.exec")
-    })
-
-    doLast {
-        println("✅ Jacoco report generated: ${reports.html.outputLocation.get()}/index.html")
-    }
 }
