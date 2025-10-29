17c982dc8153c55bee54c3190c80a6a066e8dc66
vtd182 <21120432@student.hcmus.edu.vn>
2025-10-29 09:50:34 +0700

    small pics

--- app/src/main/java/com/example/myimageloaderproject/modules/home/presentation/adapter/PhotoAdapter.kt
+++ app/src/main/java/com/example/myimageloaderproject/modules/home/presentation/adapter/PhotoAdapter.kt
@@ -60,7 +60,7 @@ class PhotoAdapter(
             val ratio = 1
             val itemHeight = (itemWidth * ratio).toInt()
 
-            photo.urls.raw?.let {
+            photo.urls.small?.let {
                 val request = ImageLoader.with(imgPhoto.context)
                     .overrideSize(itemWidth, itemHeight)
                     .placeholder(photo.color)
